daa8a112cdf7d8feea59981747f172ba242cb655
Gideon Atauba <ataubagideon@gmail.com>
2024-03-10 12:44:46 +0100

    updated Product.jsx

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -2,19 +2,19 @@ import React from 'react'
 import { useEffect, useState } from 'react'
 import classes from './Product.module.css'
 import Modal from './Modal'
-import Backdrop from '@material-ui/core/Backdrop'
-import CircularProgress from '@material-ui/core/CircularProgress'
-import Button from '@material-ui/core/Button'
-import { makeStyles } from '@material-ui/core/styles'
-const useStyles = makeStyles((theme) => ({
-  backdrop: {
-    zIndex: theme.zIndex.drawer + 1,
-    color: '#fff',
-  },
-}))
+// import Backdrop from '@material-ui/core/Backdrop'
+// import CircularProgress from '@material-ui/core/CircularProgress'
+// import Button from '@material-ui/core/Button'
+// import { makeStyles } from '@material-ui/core/styles'
+// const useStyles = makeStyles((theme) => ({
+//   backdrop: {
+//     zIndex: theme.zIndex.drawer + 1,
+//     color: '#fff',
+//   },
+// }))
 
 export default function Products() {
-  const classStyle = useStyles()
+  // const classStyle = useStyles()
   const url = 'https://fakestoreapi.com/products'
   const [products, setProducts] = useState(null)
   const [selectedProduct, setSelectedProduct] = useState([])
@@ -86,10 +86,10 @@ export default function Products() {
                 </div>
               </div>
             ))
-          ) : (
-            <Backdrop className={classStyle.backdrop} open>
-              <CircularProgress color='inherit' />
-            </Backdrop>
+          ) : (<p>Loading...</p>
+            // <Backdrop className={classStyle.backdrop} open>
+            //   <CircularProgress color='inherit' />
+            // </Backdrop>
           )}
         </div>
       </div>
